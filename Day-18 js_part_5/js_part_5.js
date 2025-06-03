8c62c210b8be5149b531d8c034ddc2767cc2454a
RanokFerdous <164327286+RanokFerdous@users.noreply.github.com>
2025-06-03 10:20:34 +0600

    Add files via upload

---
+++ Day-18 js_part_5/js_part_5.js
@@ -0,0 +1,43 @@
+let delhi ={
+    latitude :"28.70041 N",
+    longitude : "77.1025 E"
+};
+
+const student ={
+    name: "sharda",
+    age:23,
+    marks:94.4,
+    city:"Delhi"
+};
+
+
+const item ={
+    price: 100.32,
+    discount:50,
+    colors:["red","pink"]
+}
+
+//   properties: (key, value)
+
+//key is the name.
+
+
+// class: 03  creating a post
+
+const post ={
+    username:"@shardakhapra",
+    content: "this is my #first psot",
+    likes: 159,
+    reposts: 5,
+    tags:["@apmacollege","@meta"]
+};
+
+
+
+//class: 4 get value
+
+let students ={
+    name: "sharda",
+    marks: 94.4
+};
+  
